12cf36bae188b0b7ec2a38ed712031fa68e5b588
Themanual22 <djamosry22@gmail.com>
2022-05-19 07:55:09 -0400

    commit tree 6f41d72026a146e38461ffb69d3987965c182a11

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
-console.log(message);
+// module.exports = {name, height, message};
+// console.log(message);
+var name = "Joe"
+var height = 74
+var message = ` is ${height} inches tall`
+
+module.exports = {name, height, message}
